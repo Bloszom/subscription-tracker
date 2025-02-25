d918bd134c8aed5ef72f702d3acb81fd8167b412
Bloszom <blossom.giane@decagon.dev>
2025-02-25 03:14:42 +0100

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World i am new here");
+import express from "express";
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+     res.send("Welcome to the sub-tracker API")
+});
+
+app.listen(3000, () => {
+    console.log("Sub tracker API is running on http://localhost:3000")
+})
+
+export default app;
