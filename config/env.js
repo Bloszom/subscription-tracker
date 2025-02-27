203bdb7fada5c24a380b4833ef3ed4585541e89d
Bloszom <blossom.giane@decagon.dev>
2025-02-27 03:21:34 +0100

    configure arcjet to handle bots and keep your apis safe and to monitor how users are using ur website

--- config/env.js
+++ config/env.js
@@ -5,4 +5,9 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 config({ path: `config/.env.${NODE_ENV}.local` });
 
 console.log(`Environment: ${NODE_ENV}`); // Debugging
-export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const { PORT,
+    DB_URI,
+    JWT_SECRET,
+    JWT_EXPIRES_IN,
+    ARCJET_ENV,
+    ARCJET_KEY } = process.env;
