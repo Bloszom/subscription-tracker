7f61d480d66818eb142aabb615076f45e797758a
Bloszom <blossom.giane@decagon.dev>
2025-02-27 01:03:00 +0100

    authentication with JWT token and setting up of signup and signin

--- config/env.js
+++ config/env.js
@@ -1,12 +1,8 @@
-// import {config} from 'dotenv';
-//
-// config({path:`.env.${process.env.NODE_ENV || 'development'}.local`});
-//
-// export const {PORT, NODE_ENV, DB_URI} = process.env;
+
 import { config } from 'dotenv';
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 config({ path: `config/.env.${NODE_ENV}.local` });
 
 console.log(`Environment: ${NODE_ENV}`); // Debugging
-export const { PORT, DB_URI } = process.env;
+export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
